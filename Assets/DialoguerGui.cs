a95a18b6f7a284a825427663a556b961efde919d
TaniaFilonenko <tetiana.filonenko2012@gmail.com>
2014-11-09 02:07:46 +0200

    init commit

---
+++ Assets/DialoguerGui.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialoguerGui : MonoBehaviour {
+	private bool _showing;
+	private string[] _choices;
+	private string _text;
+
+	// Use this for initialization
+	void Start () {
+		Dialoguer.events.onStarted += onStarted;
+		Dialoguer.events.onEnded += onEnded;
+		Dialoguer.events.onTextPhase += onTextPhase;
+	}
+	void OnGUI()
+	{
+		if (!_showing) 
+			return;
+				
+
+						GUI.Box (new Rect (10, 10, 200, 150), "текст");
+
+			if (_choices==null){
+						if (GUI.Button (new Rect(10, 220, 200, 30), "продолжить")) {
+								Dialoguer.ContinueDialogue ();		
+						}
+				} else {
+			for (int i=0;i<_choices.Length;i++)
+			{
+			if (GUI.Button(new Rect(10,220+(40+i),200,30),_choices[i])){
+				Dialoguer.ContinueDialogue(i);
+			}
+			}
+		}
+		}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void onStarted()
+	{
+		_showing = true;
+	}
+	private void onEnded()
+	{
+		_showing = false;
+	}
+	private void onTextPhase(DialoguerTextData data){
+
+		_text = data.text;
+		_choices = data.choices;
+	}
+
+}
---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/changeAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeAnimation : MonoBehaviour {
+	protected Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) { 
+						animator.SetBool ("isWalk", true);
+				} else {
+			animator.SetBool ("isWalk", false);
+				}
+	}
+}
